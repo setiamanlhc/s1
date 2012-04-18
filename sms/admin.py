0e53be3eedae6d275830ed653f1c8c01b10d90a6
setiaman <setiaman.lee@gmail.com>
2012-04-17 23:34:37 -0400

    add send sms module

--- sms/admin.py
+++ sms/admin.py
@@ -1,8 +1,10 @@
 #Admin.py
 #Developed by Setiaman, 09-04-2012
-
+from datetime import datetime
 from django.contrib import admin
 from models import Contact, Sender, Group, Country
+from models import SMS
+from django import forms
 
 class ModelManager(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
@@ -18,8 +20,12 @@ class ContactAdmin(ModelManager):
  
     def queryset(self, request):
         if request.user.is_superuser:
-            return Contact.objects.all()
-        return Contact.objects.filter(user=request.user)
+            #return Contact.objects.all()
+            return self.model._default_manager.all()
+        return self.model._default_manager.filter(user=request.user)
+    
+#        return Contact.objects.filter(user=request.user)
+
 
 class SenderAdmin(ModelManager):
     exclude = ('user',)
@@ -48,10 +54,39 @@ class GroupAdmin(ModelManager):
         form.base_fields['members'].queryset = form.base_fields['members'].queryset.filter(user=request.user)
         return form
     
+class SMSAdminForm(forms.ModelForm):
+    class Meta:
+        model = SMS
+
         
+
+    
+class SMSAdmin(ModelManager):
+    #form = SMSAdminForm
+    exclude = ('user','contacts')
+    list_display = ('sender','senton',)
+    search_fields = ['sender']
+    filter_horizontal = ('groups',)
+
+    def save_formset(self, request, form, formset, change):
+        instances = formset.save(commit=True)
+        for instance in instances:
+            instance.user = request.user
+            instance.save()
+        formset.save_m2m()
+        
+    
+"""
+    def queryset(self, request):
+        if request.user.is_superuser:
+            return SMS.objects.all()
+        return SMS.objects.filter(user=request.user)    
+"""        
 
 admin.site.register(Country)
 admin.site.register(Contact, ContactAdmin)
 admin.site.register(Sender, SenderAdmin)
 admin.site.register(Group, GroupAdmin)
+admin.site.register(SMS, SMSAdmin)
+
 
--- sms/models.py
+++ sms/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
 class Country(models.Model):
@@ -45,3 +46,26 @@ class Group(models.Model):
     def __unicode__(self):
         return self.name
 
+class SMS(models.Model):
+    sender = models.ForeignKey(Sender)
+    user = models.ForeignKey(User)
+    message = models.CharField('Message', max_length=160, help_text='enter @SN to replace it with short name of contact')
+    groups = models.ManyToManyField(Group, blank=True, null=True)
+    contacts = models.ManyToManyField(Contact,blank=True, null=True)
+    senton = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = "SMS"
+        verbose_name = _('SMS')
+        verbose_name_plural = _('Send SMS')
+        
+    def __unicode__(self):
+        return self.message
+"""
+    def clean(self):
+        from django.core.exceptions import ValidationError
+        # Don't allow draft entries to have a pub_date.
+        if self.contacts is None and self.groups is None:
+            raise ValidationError('Either Group or Contact is empty')
+   """     
+            
